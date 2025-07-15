c58b4fd0dfaf5071c9d52f58efeeb3e2f398e2e7
Furqan Uppal <92501915+furqanuppal@users.noreply.github.com>
2025-07-15 15:57:13 +0500

    Update readme_generator.py

--- agent/readme_generator.py
+++ agent/readme_generator.py
@@ -1,1 +1,12 @@
+def generate_readme(project_summary, file_summaries):
+    readme = '# Project Overview\n\n'
+    readme += f'{project_summary}\n\n'
 
+    readme += '# File Summaries\n'
+
+    for path, summary in file_summaries.items():
+        short_path = path.split('/')[-1]
+
+        readme += f'### {short_path}\n{summary}\n\n'
+
+    return readme
