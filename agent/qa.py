4c1c18a34d424c3b9b0a182a747244a483ca34b0
Furqan Uppal <92501915+furqanuppal@users.noreply.github.com>
2025-07-21 10:52:45 +0500

    Create qa.py

---
+++ agent/qa.py
@@ -0,0 +1,25 @@
+from agent.analyzer import client, deployment_name
+
+def answer_question(question, file_summaries):
+    combined = "\n".join(
+        f"{path}:\n{summary}" for path, summary in file_summaries.items()
+    )
+
+    prompt = f"""
+Your are a smart AI code assistant. Based on the following code summaries, answer the user's question.
+
+Codebase summaries:
+{combined[:500000000]}
+
+Question:
+{question}
+"""
+    
+    response = client.chat.completions.create(
+        model = deployment_name,
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0.3,
+        max_tokens=700
+    )
+
+    return response.choices[0].message.content.strip()
